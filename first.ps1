00510f2d63646571512559d303ef236fa8a8e0a1
Anthony Spiteri <anthony@sliema.com.au>
2020-09-21 23:04:22 +0800

    Update first.ps1

--- first.ps1
+++ first.ps1
@@ -5,3 +5,7 @@ set-NetFirewallProfile -All -Enabled False
 $password = ConvertTo-SecureString 'Veeam1!' -AsPlainText -Force
 new-localuser -Name autodeploy -Password $password
 add-localgroupmember -Group administrators -Member autodeploy
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install cygwin cyg-get -y
+cyg-get openssh python38 python38-pip python38-devel libssl-devel libffi-devel gcc-g++
