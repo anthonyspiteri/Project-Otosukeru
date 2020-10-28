07e7b8491deee5f7dcd44d63b78ac7bfb036071a
Anthony Spiteri <anthony@sliema.com.au>
2020-10-28 18:31:31 +0800

    Update disk_creation.ps1

--- disk_creation.ps1
+++ disk_creation.ps1
@@ -8,10 +8,10 @@ $disk = Get-Disk -Number 2
 Set-Disk -InputObject $disk -IsOffline $false
 Initialize-Disk -InputObject $disk
 New-Partition $disk.Number -UseMaximumSize -DriveLetter R
-Format-Volume -DriveLetter V -FileSystem ReFS -AllocationUnitSize 65536 -NewFileSystemLabel "REPO-01" -Confirm:$false
+Format-Volume -DriveLetter R -FileSystem ReFS -AllocationUnitSize 65536 -NewFileSystemLabel "REPO-01" -Confirm:$false
 
 $disk = Get-Disk -Number 3
 Set-Disk -InputObject $disk -IsOffline $false
 Initialize-Disk -InputObject $disk
 New-Partition $disk.Number -UseMaximumSize -DriveLetter S
-Format-Volume -DriveLetter V -FileSystem ReFS -AllocationUnitSize 65536 -NewFileSystemLabel "REPO-02" -Confirm:$false
+Format-Volume -DriveLetter S -FileSystem ReFS -AllocationUnitSize 65536 -NewFileSystemLabel "REPO-02" -Confirm:$false
